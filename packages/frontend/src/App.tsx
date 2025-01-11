ea034d26c939baa8bbc84c79994876bd33f1f55f
Mikael Marceniuk <mika.marceniuk@gmail.com>
2025-01-11 08:44:21 -0300

    feat(Frontend): Implement shadcn-ui

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormControl, FormField, FormItem } from './components/ui/form'
+import { Input } from './components/ui/input'
+import { Button } from './components/ui/button'
+import { Search } from 'lucide-react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const formSchema = z.object({
+  query: z.string(),
+})
+
+type SearchForm = z.infer<typeof formSchema>
+
+const App = () => {
+  const form = useForm<SearchForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      query: '',
+    },
+  })
+
+  const handleOnSubmit = form.handleSubmit(({ query }) => {
+    console.log(`Searching for ${query}`)
+  })
 
   return (
-    <>
+    <div className="h-dvh bg-background">
+      <h1>Minhas Tarefas</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Form {...form}>
+          <form
+            className="flex gap-2"
+            onSubmit={handleOnSubmit}
+          >
+            <FormField
+              control={form.control}
+              name="query"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      placeholder="Procurar por tarefas..."
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button>
+              <Search strokeWidth={4} />
+            </Button>
+          </form>
+        </Form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
